0f9e61f514a7e59b086ae6860beff7e9780197aa
Andreas Bourakis <bourakis@gmail.com>
2020-11-17 13:50:26 +0200

    Update BooleanArrayPrint.java

--- Exercises/Part 1/BooleanArrayPrint.java
+++ Exercises/Part 1/BooleanArrayPrint.java
@@ -2,31 +2,29 @@ public class BooleanArrayPrint
 {
 	
 	public static void main(String[] args) 
-    {
+	{
 		boolean[][] array = {{true, false, true},
-							 {false, true, false}};
+				     {false, true, false}};
 		printArray(array);
 	}
-	
+
 	private static void printArray(boolean[][] array) 
-    {
+	{
 		for (int i = 0; i < array.length; i++) 
-        {
+		{
 			for (int j = 0; j < array[i].length; j++) 
-            {
+			{
 				if (array[i][j]) 
-                {
+				{
 					System.out.print("*");
 				} 
-                else 
-                {
+				else 
+				{
 					System.out.print(" ");
 				}
 			}
-            
+
 			System.out.println();
 		}
-		
 	}
-	
 }
