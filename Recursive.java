8de35d786281f3c2cd058ba5be8359850e513f7d
Andreas Bourakis <bourakis@gmail.com>
2021-11-19 10:39:53 +0200

    Add files via upload

---
+++ Recursive.java
@@ -0,0 +1,27 @@
+// A Java program to demonstrate working of
+// recursion
+
+public class Recursive
+{
+    public static void printFun(int test)
+    {
+        if (test < 1)
+            return;
+        else
+        {
+            System.out.printf("%d ", test);
+            printFun(test - 1); // statement 2
+
+            System.out.printf("%d ", test);
+
+            return;
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int test = 3;
+        printFun(test);
+    }
+}
