e08ff5d498ae0ddbbb208173d1e2291c834aa41c
Andreas Bourakis <bourakis@gmail.com>
2021-12-02 22:01:43 +0200

    Create RecursiveExample2.java

---
+++ Recursive/RecursiveExample2.java
@@ -0,0 +1,20 @@
+public class RecursiveExample2
+{
+  public static void main(String[] args) 
+  {
+    int result = sum(10);
+    System.out.println(result);
+  }
+  
+  public static int sum(int k) 
+  {
+    if (k > 0) 
+    {
+      return k + sum(k - 1);
+    } 
+    else 
+    {
+      return 0;
+    }
+  }
+}
