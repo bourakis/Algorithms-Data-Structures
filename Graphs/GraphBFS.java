b1aa1db2d1a9f1bc66a18cc34a93afcb5cd51f3e
Andreas Bourakis <bourakis@gmail.com>
2021-12-10 11:58:53 +0200

    Update GraphBFS.java

--- Graphs/GraphBFS.java
+++ Graphs/GraphBFS.java
@@ -12,6 +12,7 @@ class GraphBFS
     { 
         Vertices = v; 
         adj_list = new LinkedList[v]; 
+        
         for (int i=0; i<v; ++i)         //create adjacency lists
             adj_list[i] = new LinkedList(); 
     } 
@@ -32,19 +33,23 @@ class GraphBFS
         LinkedList<Integer> queue = new LinkedList<Integer>(); 
    
         // current node = visited, insert into queue 
-        visited[root_node]=true; 
+        visited[root_node] = true; 
         queue.add(root_node); 
    
-        while (queue.size() != 0)  { 
+        while (queue.size() != 0)  
+        { 
             // deque an entry from queue and process it  
             root_node = queue.poll(); 
-            System.out.print(root_node+" "); 
+            System.out.print(root_node + " "); 
    
             // get all adjacent nodes of current node and process
             Iterator<Integer> i = adj_list[root_node].listIterator(); 
-            while (i.hasNext()){ 
+            while (i.hasNext())
+            { 
                 int n = i.next(); 
-                if (!visited[n]) { 
+                
+                if (!visited[n]) 
+                { 
                     visited[n] = true; 
                     queue.add(n); 
                 } 
@@ -59,14 +64,17 @@ class GraphBFS
     { 
         //create a graph with 5 vertices
         GraphBFS g = new GraphBFS(5); 
+        
         //add edges to the graph  
         g.addEdge(0, 1); 
         g.addEdge(0, 2); 
         g.addEdge(0, 3); 
         g.addEdge(1, 2); 
         g.addEdge(2, 4); 
+        
         //print BFS sequence
         System.out.println("Breadth-first traversal of graph with 0 as starting vertex:"); 
+        
         g.BFS(0); 
     } 
 }
