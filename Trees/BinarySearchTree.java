2544ab2a2e1c9a9a3546556bc4517fd72899b857
Andreas Bourakis <bourakis@gmail.com>
2021-12-16 22:38:14 +0200

    Update BinarySearchTree.java

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -1,12 +1,14 @@
-    public class BinarySearchTree {  
-      
+    public class BinarySearchTree
+    {  
         //Represent a node of binary tree  
-        public static class Node{  
+        public static class Node
+        {  
             int data;  
             Node left;  
             Node right;  
       
-            public Node(int data){  
+            public Node(int data)
+            {  
                 //Assign data to the new node, set left and right children to null  
                 this.data = data;  
                 this.left = null;  
@@ -17,40 +19,51 @@
           //Represent the root of binary tree  
           public Node root;  
       
-          public BinarySearchTree(){  
+          public BinarySearchTree()
+          {  
               root = null;  
           }  
       
           //insert() will add new node to the binary search tree  
-          public void insert(int data) {  
+          public void insert(int data) 
+          {  
               //Create a new node  
               Node newNode = new Node(data);  
       
               //Check whether tree is empty  
-              if(root == null){  
+              if(root == null)
+              {  
                   root = newNode;  
                   return;  
-                }  
-              else {  
+              }  
+              else 
+              {  
                   //current node point to root of the tree  
                   Node current = root, parent = null;  
       
-                  while(true) {  
+                  while(true) 
+                  {  
                       //parent keep track of the parent node of current node.  
                       parent = current;  
       
                       //If data is less than current's data, node will be inserted to the left of tree  
-                      if(data < current.data) {  
-                          current = current.left;  
-                          if(current == null) {  
+                      if(data < current.data) 
+                      {  
+                          current = current.left;
+                          
+                          if(current == null) 
+                          {  
                               parent.left = newNode;  
                               return;  
                           }  
                       }  
                       //If data is greater than current's data, node will be inserted to the right of tree  
-                      else {  
+                      else 
+                      {  
                           current = current.right;  
-                          if(current == null) {  
+                          
+                          if(current == null) 
+                          {  
                               parent.right = newNode;  
                               return;  
                           }  
@@ -60,7 +73,8 @@
           }  
       
           //minNode() will find out the minimum node  
-          public Node minNode(Node root) {  
+          public Node minNode(Node root) 
+          {  
               if (root.left != null)  
                   return minNode(root.left);  
               else  
@@ -68,11 +82,14 @@
           }  
       
           //deleteNode() will delete the given node from the binary search tree  
-          public Node deleteNode(Node node, int value) {  
-              if(node == null){  
+          public Node deleteNode(Node node, int value) 
+          {  
+              if(node == null)
+              {  
                   return null;  
-               }  
-              else {  
+              }  
+              else 
+              {  
                   //value is less than node's data then, search the value in left subtree  
                   if(value < node.data)  
                       node.left = deleteNode(node.left, value);  
@@ -82,23 +99,25 @@
                       node.right = deleteNode(node.right, value);  
       
                   //If value is equal to node's data that is, we have found the node to be deleted  
-                  else {  
+                  else 
+                  {  
                       //If node to be deleted has no child then, set the node to null  
                       if(node.left == null && node.right == null)  
                           node = null;  
       
                       //If node to be deleted has only one right child  
-                      else if(node.left == null) {  
+                      else if(node.left == null) 
+                      {  
                           node = node.right;  
                       }  
       
                       //If node to be deleted has only one left child  
-                      else if(node.right == null) {  
+                      else if(node.right == null) 
+                      {  
                           node = node.left;  
                       }  
-      
-                      //If node to be deleted has two children node  
-                      else {  
+                      else //If node to be deleted has two children node
+                      {  
                           //then find the minimum node from right subtree  
                           Node temp = minNode(node.right);  
                           //Exchange the data between node and temp  
@@ -107,32 +126,37 @@
                           node.right = deleteNode(node.right, temp.data);  
                       }  
                   }  
+                  
                   return node;  
               }  
           }  
       
           //inorder() will perform inorder traversal on binary search tree  
-          public void inorderTraversal(Node node) {  
-      
+          public void inorderTraversal(Node node) 
+          {  
               //Check whether tree is empty  
-              if(root == null){  
+              if(root == null)
+              {  
                   System.out.println("Tree is empty");  
                   return;  
-               }  
-              else {  
-      
+              }  
+              else 
+              {  
                   if(node.left!= null)  
                       inorderTraversal(node.left);  
+                  
                   System.out.print(node.data + " ");  
+                  
                   if(node.right!= null)  
                       inorderTraversal(node.right);  
       
               }  
           }  
       
-          public static void main(String[] args) {  
-      
+          public static void main(String[] args) 
+          {  
               BinarySearchTree bt = new BinarySearchTree();  
+              
               //Add nodes to the binary tree  
               bt.insert(50);  
               bt.insert(30);  
@@ -146,6 +170,7 @@
               bt.inorderTraversal(bt.root);  
       
               Node deletedNode = null;  
+              
               //Deletes node 90 which has no child  
               deletedNode = bt.deleteNode(bt.root, 90);  
               System.out.println("\nBinary search tree after deleting node 90:");  
