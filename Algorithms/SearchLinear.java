2b28d62315d0116b68131aeb864aa69a25d3f90f
Andreas Bourakis <bourakis@gmail.com>
2021-12-16 19:40:48 +0200

    Create SearchLinear.java

---
+++ Algorithms/SearchLinear.java
@@ -0,0 +1,43 @@
+package searchlinear;
+
+public class SearchLinear 
+{
+    public static void main(String[] args) 
+    {
+		//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+		// Εύρεση της θέσης ενός αριθμού που ψάχνουμε μέσα σε ένα πίνακα:
+		//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        int[] array3 = {3, 2, 8, 12, 5, 34, 67};
+        int stoxos = 34; // ο αριθμός που θα αναζητήσουμε
+        int result = linearSearch(stoxos, array3);
+    }   
+    
+    public static int linearSearch(int target, int[] array)
+    {
+        int position = -1;
+      
+        for (int i = 0; i<array.length; i++)
+        {
+            if (array[i] == target)
+            {
+                position = i;
+                break;	// δεν έχει νόημα να συνεχίσουμε το ψάξιμο αφού ο αριθμός βρέθηκε!
+            }
+        }
+      
+        if (position==-1)
+        {
+            System.out.println("Ο αριθμός δε βρέθηκε!");
+        }
+        else 
+        {
+            System.out.println("Η θεση είναι η: "+(position+1));
+        }
+      
+        return position;
+    }
+    
+    public static int binarySearch(int target, int[] array){
+        
+    }
+}
