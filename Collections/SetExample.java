31094b987b20a84f07f9e8f1dbca65ce3ef631c6
Andreas Bourakis <bourakis@gmail.com>
2021-12-17 18:33:15 +0200

    Update SetExample.java

--- Collections/SetExample.java
+++ Collections/SetExample.java
@@ -18,11 +18,11 @@ public class SetExample
 		a.addAll(Arrays.asList(
 			new Integer[] { 1, 3, 2, 4, 8, 9, 0 }));
 	
-	  // Again declaring object of Set class
-	  // with reference to HashSet
+	  	// Again declaring object of Set class
+	  	// with reference to HashSet
 		Set<Integer> b = new HashSet<Integer>();
 		
-	b.addAll(Arrays.asList(
+		b.addAll(Arrays.asList(
 			new Integer[] { 1, 3, 7, 5, 4, 0, 7, 5 }));
 
 		
