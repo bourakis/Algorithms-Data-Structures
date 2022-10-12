336e8064fa8733833bde958d7bb9a22758276ad1
Andreas Bourakis <bourakis@gmail.com>
2022-10-12 23:00:01 +0300

    Create Array2D.java

---
+++ Arrays/Array2D.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Array2D 
+{
+  public static void main(String[] args) 
+  {      
+    int [][] array2 = {{2,4,6},{8,9,1}};
+
+    for (int i = 0; i < array2.length; i++)
+    {
+      for (int j = 0; j < array2[i].length; j++)
+      {
+
+      }
+    }
+  }
+}
