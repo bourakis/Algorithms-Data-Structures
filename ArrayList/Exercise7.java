a17b5d4bd9ce17b22f67d255ff161e6d3b7018ec
Andreas Bourakis <bourakis@gmail.com>
2022-10-20 23:56:13 +0300

    Update Exercise7.java

--- ArrayList/Exercise7.java
+++ ArrayList/Exercise7.java
@@ -7,16 +7,16 @@ public class Exercise7
   public static void main(String[] args) 
   {
     // Creae a list and add some colors to the list
-    List<String> list_Strings = new ArrayList<String>();
+    List<String> myList = new ArrayList<String>();
 
-    list_Strings.add("Red");
-    list_Strings.add("Green");
-    list_Strings.add("Orange");
-    list_Strings.add("White");
-    list_Strings.add("Black");
+    myList.add("Red");
+    myList.add("Green");
+    myList.add("Orange");
+    myList.add("White");
+    myList.add("Black");
 
     // Search the value Red
-    if (list_Strings.contains("Red")) 
+    if (myList.contains("Red")) 
     {
       System.out.println("Found the element");
     } 
