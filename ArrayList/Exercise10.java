5efdd417ba6b4b0d0c086bb2084dddc6684a20b3
Andreas Bourakis <bourakis@gmail.com>
2022-10-20 23:58:40 +0300

    Update Exercise10.java

--- ArrayList/Exercise10.java
+++ ArrayList/Exercise10.java
@@ -7,19 +7,19 @@ public class Exercise10
 	public static void main(String[] args) 
 	{
 		// Creae a list and add some colors to the list
-		List<String> list_Strings = new ArrayList<String>();
+		List<String> myList = new ArrayList<String>();
 
-		list_Strings.add("Red");
-		list_Strings.add("Green");
-		list_Strings.add("Orange");
-		list_Strings.add("White");
-		list_Strings.add("Black");
+		myList.add("Red");
+		myList.add("Green");
+		myList.add("Orange");
+		myList.add("White");
+		myList.add("Black");
 
-		System.out.println("List before shuffling:\n" + list_Strings);  
+		System.out.println("List before shuffling:\n" + myList);  
 
-		Collections.shuffle(list_Strings);
+		Collections.shuffle(myList);
 		
-		System.out.println("List after shuffling:\n" + list_Strings); 
+		System.out.println("List after shuffling:\n" + myList); 
 	}
 }
 
