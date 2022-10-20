13ad3eac4d0775dc7903bd68dce00834f00c76d1
Andreas Bourakis <bourakis@gmail.com>
2022-10-20 23:55:14 +0300

    Update Exercise5.java

--- ArrayList/Exercise5.java
+++ ArrayList/Exercise5.java
@@ -8,21 +8,21 @@ public class Exercise5
 	public static void main(String[] args) 
 	{
 		// Creae a list and add some colors to the list
-		List<String> list_Strings = new ArrayList<String>();
+		List<String> myList = new ArrayList<String>();
 
-		list_Strings.add("Red");
-		list_Strings.add("Green");
-		list_Strings.add("Orange");
-		list_Strings.add("White");
-		list_Strings.add("Black");
+		myList.add("Red");
+		myList.add("Green");
+		myList.add("Orange");
+		myList.add("White");
+		myList.add("Black");
 
 		// Print the list
-		System.out.println(list_Strings);
+		System.out.println(myList);
 
 		// Update the third element with "Yellow"
-		list_Strings.set(2, "Yellow");
+		myList.set(2, "Yellow");
 		
 		// Print the list again
-		System.out.println(list_Strings);
+		System.out.println(myList);
 	}
 }
