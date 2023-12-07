cd5ca8cb67f348e2d55ca31dcc79d786c52f7d6c
Andreas Bourakis <bourakis@gmail.com>
2023-12-07 12:37:31 +0200

    Update GraphDFSv1.java

--- Graphs/GraphDFSv1.java
+++ Graphs/GraphDFSv1.java
@@ -2,46 +2,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class GraphDFSv1 {
+public class GraphDFSv1 
+{
     private int numVertices;
     private List<Integer>[] adjacencyLists;
     private boolean[] visited;
 
-    public GraphDFSv1(int numVertices) {
+    public GraphDFSv1(int numVertices) 
+    {
         this.numVertices = numVertices;
         adjacencyLists = (List<Integer>[]) new ArrayList[numVertices];
 
-        for (int i = 0; i < numVertices; i++) {
+        for (int i = 0; i < numVertices; i++) 
+        {
             adjacencyLists[i] = new ArrayList<>();
         }
     }
 
-    public void addEdge(int u, int v) {
+    public void addEdge(int u, int v) 
+    {
         adjacencyLists[u].add(v);
     }
 
-    public void dfs(int startVertex) {
+    public void dfs(int startVertex) 
+    {
         visited = new boolean[numVertices];
         Stack<Integer> stack = new Stack<>();
 
         stack.push(startVertex);
 
-        while (!stack.isEmpty()) {
+        while (!stack.isEmpty()) 
+        {
             int vertex = stack.pop();
 
-            if (!visited[vertex]) {
+            if (!visited[vertex]) 
+            {
                 visited[vertex] = true;
                 System.out.print(vertex + " ");
 
-                for (int neighbor : adjacencyLists[vertex]) {
+                for (int neighbor : adjacencyLists[vertex]) 
+                {
                     stack.push(neighbor);
                 }
             }
-
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         GraphDFSv1 g = new GraphDFSv1(6);
 
         g.addEdge(0, 1);
