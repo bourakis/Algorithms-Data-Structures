a5a9b01322202c8d34fd736bff46177e216c00a3
Andreas Bourakis <bourakis@gmail.com>
2024-01-18 13:24:21 +0200

    Update SelectionSortTest.java

--- Algorithms/SelectionSortTest.java
+++ Algorithms/SelectionSortTest.java
@@ -1,5 +1,5 @@
 // Java program for implementation of Selection Sort
-class SelectionSort
+class SelectionSortTest
 {
     void sort(int arr[])
     {
@@ -28,16 +28,19 @@ class SelectionSort
    	 int n = arr.length;
    	 for (int i=0; i<n; ++i)
    		 System.out.print(arr[i]+" ");
+        
    	 System.out.println();
     }
 
     // Driver code to test above
     public static void main(String args[])
     {
-   	 SelectionSort ob = new SelectionSort();
+   	 SelectionSortTest ob = new SelectionSortTest();
+        
    	 int arr[] = {64,25,12,22,11};
    	 ob.sort(arr);
    	 System.out.println("Sorted array");
+        
    	 ob.printArray(arr);
     }
 }  
